769cfbc54b3069752d89e5db435ad07b6324e6f9
Himanshu Jain <himanshujain.2792@gmail.com>
2017-05-15 16:22:14 +0530

    api for graph 6-8

---
+++ models/student_competency.js
@@ -0,0 +1,47 @@
+/* jshint indent: 2 */
+
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define("student_competency", {
+    id: {
+      type: DataTypes.INTEGER(20),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    competency: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    type: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    success_criteria: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    question: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    competency_category: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    student_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: "student",
+        key: "id"
+      }
+    },
+  }, {
+    tableName: "student_competency",
+    classMethods: {
+      associate: function (models) {
+        models.student_competency.belongsTo(models.student, {foreignKey: "student_id", targetKey: "id", as: "S"});
+      }
+    }
+  });
+};
