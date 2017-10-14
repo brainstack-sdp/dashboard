b78d85f46826272f07b2b860e52cb92e9b325b57
Himanshu <jainjainhimanshu@gmail.com>
2017-10-15 01:47:20 +0530

    fix

--- public/js/sdpapp.js
+++ public/js/sdpapp.js
@@ -22,6 +22,11 @@ HPD.urls = {
             district : ['block', 'school_name'],
             block : ['school_name'],
             school_name : []
+        },
+        filterParentMap = {
+            district : ['district'],
+            block : ['district'],
+            school_name : ['block']
         }, gradeMap = {
 
         }, gradeColors = {
@@ -117,41 +122,45 @@ HPD.urls = {
         appliedFilter.type = type;
         appliedFilter.value = $el.val();
 
-         $.ajax({
-            method: 'GET',
-            url : HPD.urls.filterList + '?' + type +'=' +encodeURIComponent($el.val()),
-            success: function(res) {
-                var key = Object.keys(res.result)[0];
-                appliedFilter.key = key
-                filterList[key] = res.result[key];
-                if(type!="school_name") {
-                    if (filterAheadMap[type]) {
-                        filterAheadMap[type].forEach(function (item) {
-                            delete filters[item];
-                            $('.js-filter[data-type="' + item + '"]').html('');
-                        })
-                        $('.js-filter[data-type="' + key + '"]').html(createOptions(filterList[key], key))
-                    } else {
-                        filterAheadMap.district.forEach(function (item) {
-                            delete filters[item];
-                            $('.js-filter[data-type="' + item + '"]').html('');
-                        })
+        if(appliedFilter.value){
+            $.ajax({
+                method: 'GET',
+                url : HPD.urls.filterList + '?' + type +'=' +encodeURIComponent($el.val()),
+                success: function(res) {
+                    var key = Object.keys(res.result)[0];
+                    appliedFilter.key = key
+                    filterList[key] = res.result[key];
+                    if(type!="school_name") {
+                        if (filterAheadMap[type]) {
+                            filterAheadMap[type].forEach(function (item) {
+                                delete filters[item];
+                                $('.js-filter[data-type="' + item + '"]').html('');
+                            })
+                            $('.js-filter[data-type="' + key + '"]').html(createOptions(filterList[key], key))
+                        } else {
+                            filterAheadMap.district.forEach(function (item) {
+                                delete filters[item];
+                                $('.js-filter[data-type="' + item + '"]').html('');
+                            })
+                        }
                     }
-                }
-                if(key == "school_name"){
-                $('.js-schools').html(createPdfList(filterList[key], key))
-                }
-                var iQuery = '&';
-                $.each(el.$iFilter, function(key, item) {
-                    if($(item).val()){
-                        iQuery += $(item).data('type') +'='+ $(item).val() + '&'
+                    if(key == "school_name"){
+                        $('.js-schools').html(createPdfList(filterList[key], key))
                     }
-                });
-                chartInit(key, type, $el.val(), iQuery);
-            }
-        });
+                    var iQuery = '&';
+                    $.each(el.$iFilter, function(key, item) {
+                        if($(item).val()){
+                            iQuery += $(item).data('type') +'='+ $(item).val() + '&'
+                        }
+                    });
+                    chartInit(key, type, $el.val(), iQuery);
+                }
+            });
+        }
 
-        if(type=="school_name") {
+
+
+        if(type=="school_name" && appliedFilter.value) {
             $.ajax({
                 method: 'GET',
                 url : HPD.urls.surveyTable + '?' + type +'=' +encodeURIComponent($el.val()),
@@ -274,7 +283,7 @@ HPD.urls = {
                                     return obj[filterKey] == item[filterKey];
                                 });
                                 if(totalSchools.length){
-                                    gradeObj.size = (item.size/totalSchools[0].size)*100;
+                                    gradeObj.size = Math.floor((item.size/totalSchools[0].size)*100);
                                     if(gradeObj.size>100) {
                                         gradeObj.size = 100;
                                     }
@@ -1215,7 +1224,13 @@ HPD.urls = {
         for(var key in pendingCalls){
             pendingCalls[key].fail();
         }
-        loadFilters($(this));
+        var $this = $(this), parent =  $this.attr('data-type')
+        if($this.val()){
+            loadFilters($(this));
+        } else {
+            loadFilters($('[data-type="'+filterParentMap[parent]+'"]'));
+        }
+
     });
     el.$iFilter.on('change', function() {
         var iQuery = '&';
